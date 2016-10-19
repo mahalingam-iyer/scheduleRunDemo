68350a51e0f7c6c955f944f90dcd90b890b2103e
Mahalingam Iyer <mahalingam.m.iyer@gmail.com>
2016-10-19 08:21:57 +0530

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,99 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('run', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller('main', function ($scope, $ionicModal, $http) {
+  var map;
+  var currentLocation;
+  var api = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
+  initMap();
+  $ionicModal.fromTemplateUrl('schRun.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+      $scope.newTaskModal = modal;
+      $scope.run = {};
+      // $scope.newTaskModal.show();
+  });
+  //add marker for current user location
+  function addCurrentLocMarker(pos){
+    var marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.BOUNCE,
+    position: pos
+    // icon: "assets/icon/running1.png"
+  });
+    //content
+    var address = ($scope.run.address)?$scope.run.address.data.results[0].address_components[1].long_name.toString():"address not found";
+  var content = "<h3>Scheduled Run</h3></br><h4>Time:"+$scope.run.time+"</br>Miles:"+$scope.run.miles+"</br>Address:"+address+"</h4>";          
+ 
+  bindInfoClick(marker, content);
+  }
+  //bind click event and show text on marker on map
+  function bindInfoClick(marker, content){
+    //adding info 
+    var info = new google.maps.InfoWindow({
+      content: content
+    });
+   
+    //adding click
+    google.maps.event.addListener(marker, 'click', function(){
+      info.open(map, marker);
+    });
+ 
+  }
+  $scope.createRun = function(){
+    addCurrentLocMarker(currentLocation);
+    $scope.newTaskModal.hide();
+  }
+  $scope.closeRunModal = function(){
+    $scope.newTaskModal.hide();
+  }
+  function initMap(){
+    var options = { enableHighAccuracy: true };
+    navigator.geolocation.getCurrentPosition(function(position) {
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        clickableIcons:false
+      });
+
+      google.maps.event.addListener(map, "click", function(event){
+        currentLocation = event.latLng;
+        console.log(currentLocation);
+        var url = api+currentLocation.lat()+","+currentLocation.lng();
+        console.log(url);
+        $http.get(url).then(function(data){
+          $scope.run.address = data;
+          $scope.newTaskModal.show();
+        },function(){
+          $scope.run.address = null;
+        })
+      });
+    },function(err) {
+      console.log(err);
+    },options);
+  }
+});
